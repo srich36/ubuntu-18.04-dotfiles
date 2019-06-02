4d8c88c7196d87ff505373b8b2e0a52dcae47484
Sean Rich <srich366@gmail.com>
2019-06-01 20:50:32 -0400

    Configure i3 and polybar for multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,16 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+    MONITOR=$m polybar --reload bottom &
+  done
+else
+  polybar --reload top &
+  polybar --reload bottom &
+fi
 # Launch bar1 and bar2
-polybar bottom &
-polybar top &
+#polybar bottom &
+#polybar top &
 echo "Bars launched..."
